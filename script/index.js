6c34cb5dee9da0a22bc6363228a56c80abd103b3
Durgesh Mishra <67376628+Shaktiman-Bro@users.noreply.github.com>
2026-04-17 16:27:16 +0530

    Add Some images and css files

---
+++ script/index.js
@@ -0,0 +1,7 @@
+const downloadButton = document.getElementById('download-btn')
+
+function downloadBtn() {
+    console.log("Pdf is downloading....")
+}
+
+downloadButton.addEventListener('click', downloadBtn)
